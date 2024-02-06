4a52056fb4f8087a476e6e4240d2731551719786
stimado9021 <139512881+stimado9021@users.noreply.github.com>
2024-02-06 12:13:21 -0500

    Add files via upload

--- js/encriptador.js
+++ js/encriptador.js
@@ -1,21 +1,28 @@
 const btnEncri = document.querySelector('.btn-encriptar')
+const btnDesencri = document.querySelector('.btn-desencriptar')
 
 // const removeAccents = (str) => {
 //   return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 // } 
 
+document.querySelector('#copiar').addEventListener('click',()=>{
+  var contenido = document.querySelector('#mensaResp').textContent
+ navigator.clipboard.writeText(contenido)
+ document.querySelector('#mensaResp').textContent='';
+  
+})
+
 window.addEventListener('load',()=>{
   
-    // const audio = new Audio("js/mensajeBienvenida.mp3");
-    // audio.play();
+    const audio = new Audio("js/mensajeBienvenida.mp3");
+    audio.play();
  
 })
    
 document.getElementById('texto').addEventListener('keyup',(e)=>{
   var regex = new RegExp("^[a-z ]+$");
   var key = e.target.value;
-  if (!regex.test(key)) {
-    //e.preventDefault();
+  if (!regex.test(key)) {  
    const alerta =document.querySelector(".alerta")
    alerta.style.display='block'  
    const audio = new Audio("js/mensaje.mp3");
@@ -23,51 +30,58 @@ document.getElementById('texto').addEventListener('keyup',(e)=>{
    setTimeout(()=>{
       alerta.style.display='none'       
    },8000)
-    
-
-    //return false;
   }
-  console.log(e.target.value)
+})
 
+function desencriptar(frase){
+  i=0;
+ 
+ let cifrado=frase
+      .replace(/enter/g, "e")
+      .replace(/imes/g, "i")
+      .replace(/ai/g, "a")
+      .replace(/ober/g, "o")
+      .replace(/ufat/g, "u");  
+      
+      document.querySelector('#mensaResp').innerText=cifrado;
+}
+
+
+btnDesencri.addEventListener('click',()=>{
+  const textoOriginal = JSON.parse(localStorage.getItem('frase'))
+  
+  var t = document.getElementById('texto').value  
+  
+  var palabra =desencriptar(t)
+  document.getElementById('muneco').setAttribute('src','imagen/muneco.png')            
+ 
+  localStorage.removeItem('frase')
+  const audio = new Audio("js/exitodesen.mp3");
+  audio.play();
 })
 
 btnEncri.addEventListener('click',()=>{
     var et = document.getElementById('texto')
     var st = document.querySelector('#salidaTexto')
-    var t = et.value;
-    console.log(/^([0-9])*$/.test(et.value))
-    t = t.toLowerCase();
-
-                  var cifrado='';
-                  for(var l=0;l<t.length;l++){
-
-                  switch (t[l]) {
-                          case "a":
-                              cifrado+='ai'
-                            break;
-                          case "e":
-                              cifrado+='enter'
-                            break;
-                          case "i":
-                              cifrado+='imes'
-                            break;
-                          case "o":
-                              cifrado+='ober'
-                            break;
-                          case "u":
-                              cifrado+='ufat'
-                            break;
-                            default:
-                              cifrado+=t[l];
-                      }   
-                  }
-      et.value=''
-      et.value = cifrado ;
+    var t = et.value;   
+    t = t.toLowerCase();      
+                  let cifrado=t
+                    .replace(/e/g, "enter")
+                    .replace(/i/g, "imes")
+                    .replace(/a/g, "ai")
+                    .replace(/o/g, "ober")
+                    .replace(/u/g, "ufat");
+          console.log(cifrado);
+                
+      
+     document.getElementById('mensaResp').innerText = cifrado ;
+     et.value=''
+     et.setAttribute('placeholder','Ingrese el texto aqui')     
       document.getElementById('muneco').setAttribute('src','imagen/pulgar.png')           
-      document.getElementById('mensajeDeExito').innerText='El Encriptado se ha Realizado con  Exito !Felicidades¡'
-      document.getElementById('alerta2').innerText=''
+    
       const audio = new Audio("js/mensajeExito.mp3");
       audio.play();
+      
 })
 
 
